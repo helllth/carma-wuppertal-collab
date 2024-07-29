313d027547a2e6cae990e7bb47b9bf414bee6b33
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-07-29 11:29:24 +0200

    fix links styles in introduction

--- src/e-bikes/MenuIntroduction.tsx
+++ src/e-bikes/MenuIntroduction.tsx
@@ -7,15 +7,26 @@ export const MenuIntroduction: React.FC<MenuIntroductionProps> = ({
   return (
     <span>
       Benutzen Sie die Auswahlmöglichkeiten unter{" "}
-      <a onClick={() => setAppMenuActiveMenuSection("filter")}>Filter</a>, um
-      die in der Karte angezeigten Lade- und Verleihstationen für E-Fahrräder
-      auf die für Sie relevanten Stationen zu beschränken. Über{" "}
-      <a onClick={() => setAppMenuActiveMenuSection("settings")}>
+      <a
+        onClick={() => setAppMenuActiveMenuSection("filter")}
+        className="useAClassNameToRenderProperLink"
+      >
+        Filter
+      </a>
+      , um die in der Karte angezeigten Lade- und Verleihstationen für
+      E-Fahrräder auf die für Sie relevanten Stationen zu beschränken. Über{" "}
+      <a
+        onClick={() => setAppMenuActiveMenuSection("settings")}
+        className="useAClassNameToRenderProperLink"
+      >
         Einstellungen
       </a>{" "}
       können Sie die Darstellung der Hintergrundkarte und der Stationen an Ihre
       Vorlieben anpassen. Wählen Sie{" "}
-      <a onClick={() => setAppMenuActiveMenuSection("help")}>
+      <a
+        onClick={() => setAppMenuActiveMenuSection("help")}
+        className="useAClassNameToRenderProperLink"
+      >
         Kompaktanleitung
       </a>{" "}
       für detailliertere Bedienungsinformationen.
