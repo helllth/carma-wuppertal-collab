fced0fd4acef8265843e04fcebf8574252555faf
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-07-30 12:25:46 +0200

    add Rechtsplan Und Arbeitskarte story

--- src/fnp-inspektor/_fnp-inspektor.stories.tsx
+++ src/fnp-inspektor/_fnp-inspektor.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { searchTextPlaceholder } from ".";
+import {
+  searchTextPlaceholder,
+  MenuIntroduction,
+  RechtsplanUndArbeitskarte,
+} from ".";
 
 const meta: Meta = {
   title: "Fnp-inspektor",
@@ -11,5 +15,16 @@ export default meta;
 export const TextInSearchPlaceholder: StoryObj = {
   render: () => {
     return <div>{searchTextPlaceholder}</div>;
+  },
+};
+
+export const IntroductionMenu: StoryObj = {
+  render: () => {
+    return <MenuIntroduction />;
+  },
+};
+export const RechtsplanUndArbeitskarteSection: StoryObj = {
+  render: () => {
+    return <RechtsplanUndArbeitskarte />;
   },
 };
