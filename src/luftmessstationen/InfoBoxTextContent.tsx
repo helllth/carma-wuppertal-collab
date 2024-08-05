bc180aa1472baa7f2ad628bf00dc72ac5d0fcca7
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-05 08:59:24 +0200

    fix text in InfoBoxTextContent

--- src/luftmessstationen/InfoBoxTextContent.tsx
+++ src/luftmessstationen/InfoBoxTextContent.tsx
@@ -4,8 +4,7 @@ export const InfoBoxTextContent = () => {
   return (
     <p>
       Für mehr Messstationen Ansicht mit <Icon name="minus-square" />{" "}
-      verkleinern oder mit dem untenstehenden Link alle Messstationen anzeigen
-      Themenfeldern zu filtern, das
+      verkleinern oder mit dem untenstehenden Link alle Messstationen anzeigen.
     </p>
   );
 };
