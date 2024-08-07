ee0c1c16d2437896b79dbc9adaffab8e1e2c166f
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2024-08-07 15:39:13 +0000

    deploy: 880244493a5a75b1dd87bd05e31333552118e597

--- assets/_lagis-desktop.stories-D-xqE3jE.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const r={ubersicht:"Übersicht",verwaltungsbereiche:"Verwaltungsbereiche",mipa:"Miet- und Pachtverträge",rebe:"Rechte und Belastungen",nutzung:"Nutzung",vorgange:"Vorgänge",historie:"Historie",kassenzeichen:"Kassenzeichen",dms:"DMS"},p={title:"Lagis-desktop"},i=()=>Object.keys(r).map(u=>a.jsx("p",{children:r[u]})),e={render:()=>a.jsx(i,{})};var s,n,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  render: () => {
-    return <DisplayMenu />;
-  }
-}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};const d=["MenuNamesList"];export{e as MenuNamesList,d as __namedExportsOrder,p as default};
