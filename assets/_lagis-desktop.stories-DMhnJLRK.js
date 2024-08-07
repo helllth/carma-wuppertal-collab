68f3d844f05f3dc73afa247e76b45d0a33353f98
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2024-08-07 11:56:44 +0000

    deploy: 014ddaa20946bf3988237220059fdf0da4d20080

---
+++ assets/_lagis-desktop.stories-DMhnJLRK.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const k=()=>"Übersicht",v=()=>"Verwaltungsbereiche",L=()=>"Miet- und Pachtverträge",O=()=>"Rechte und Belastungen",q=()=>"Nutzung",y=()=>"Vorgänge",C=()=>"Historie",F=()=>"Kassenzeichen",G=()=>"DMS",T={title:"Lagis-desktop"},r={render:()=>e.jsx(k,{})},n={render:()=>e.jsx(v,{})},s={render:()=>e.jsx(L,{})},t={render:()=>e.jsx(O,{})},a={render:()=>e.jsx(q,{})},u={render:()=>e.jsx(y,{})},o={render:()=>e.jsx(C,{})},c={render:()=>e.jsx(F,{})},m={render:()=>e.jsx(G,{})};var d,i,M;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameUbersicht />;
+  }
+}`,...(M=(i=r.parameters)==null?void 0:i.docs)==null?void 0:M.source}}};var p,g,N;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameVerwaltungsbereiche />;
+  }
+}`,...(N=(g=n.parameters)==null?void 0:g.docs)==null?void 0:N.source}}};var I,h,l;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameMIPA />;
+  }
+}`,...(l=(h=s.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};var S,x,j;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameREBA />;
+  }
+}`,...(j=(x=t.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var b,z,V;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameNutzung />;
+  }
+}`,...(V=(z=a.parameters)==null?void 0:z.docs)==null?void 0:V.source}}};var A,E,R;u.parameters={...u.parameters,docs:{...(A=u.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameVorgange />;
+  }
+}`,...(R=(E=u.parameters)==null?void 0:E.docs)==null?void 0:R.source}}};var w,B,D;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameHistorie />;
+  }
+}`,...(D=(B=o.parameters)==null?void 0:B.docs)==null?void 0:D.source}}};var H,K,P;c.parameters={...c.parameters,docs:{...(H=c.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameKassenzeichen />;
+  }
+}`,...(P=(K=c.parameters)==null?void 0:K.docs)==null?void 0:P.source}}};var U,f,_;m.parameters={...m.parameters,docs:{...(U=m.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: () => {
+    return <MenuNameDMS />;
+  }
+}`,...(_=(f=m.parameters)==null?void 0:f.docs)==null?void 0:_.source}}};const W=["MenuItemsUbersicht","MenuItemsVerwaltungsbereiche","MenuItemsMIPA","MenuItemsREBA","MenuItemsNutzung","MenuItemsVorgange","MenuItemsHistorie","MenuItemsKassenzeichen","MenuItemsMenuNameDMS"];export{o as MenuItemsHistorie,c as MenuItemsKassenzeichen,s as MenuItemsMIPA,m as MenuItemsMenuNameDMS,a as MenuItemsNutzung,t as MenuItemsREBA,r as MenuItemsUbersicht,n as MenuItemsVerwaltungsbereiche,u as MenuItemsVorgange,W as __namedExportsOrder,T as default};
