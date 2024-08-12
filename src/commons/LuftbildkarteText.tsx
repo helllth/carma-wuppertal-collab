9acbbb7002433bd52d9289cd8c0fb4bdb67d0b0d
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-12 17:27:51 +0200

    add props to LuftbildkarteText

--- src/commons/LuftbildkarteText.tsx
+++ src/commons/LuftbildkarteText.tsx
@@ -1,13 +1,24 @@
-const LuftbildkarteText = () => {
+interface LuftbildkarteTextProps {
+  bodenaufloesung?: number;
+  ohneKartenschrift?: string;
+  details?: string;
+}
+
+const LuftbildkarteText = ({
+  bodenaufloesung = 5,
+  ohneKartenschrift = "",
+  details = "(Details s.Hintergrundkarte Stadtplan)",
+}: LuftbildkarteTextProps) => {
   return (
     <li>
       <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) der Stadt
       Wuppertal. Datengrundlage:{" "}
       <strong>True Orthophoto aus Bildflug vom 16.03.2022</strong>, hergestellt
-      durch Aerowest GmbH/Dortmund, Bodenauflösung 5 cm. (True Orthophoto: Aus
-      Luftbildern mit hoher Längs- und Querüberdeckung in einem automatisierten
-      Bildverarbeitungsprozess berechnetes Bild in Parallelprojektion, also ohne
-      Gebäudeverkippung und sichttote Bereiche.) © Stadt Wuppertal (
+      durch Aerowest GmbH/Dortmund, Bodenauflösung {bodenaufloesung} cm. (True
+      Orthophoto: Aus Luftbildern mit hoher Längs- und Querüberdeckung in einem
+      automatisierten Bildverarbeitungsprozess berechnetes Bild in
+      Parallelprojektion, also ohne Gebäudeverkippung und sichttote Bereiche.)
+      © Stadt Wuppertal (
       <a
         target="_legal"
         href="https://www.wuppertal.de/geoportal/Nutzungsbedingungen/NB-GDIKOM-C_Geodaten.pdf"
@@ -15,9 +26,9 @@ const LuftbildkarteText = () => {
         NB-GDIKOM C
       </a>
       ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR).
-      Datengrundlagen: <strong>Stadtkarte 2.0</strong> und{" "}
-      <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. (Details s.
-      Hintergrundkarte Stadtplan).
+      Datengrundlagen: <strong>Stadtkarte 2.0</strong>
+      {ohneKartenschrift} und{" "}
+      <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. {details}.
     </li>
   );
 };
