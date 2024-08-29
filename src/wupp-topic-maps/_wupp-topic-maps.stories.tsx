0bbbb8f41a50b6eabe7cbcdd64da92094b33b690
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-29 13:50:16 +0200

    add help10Datengrundlage section

---
+++ src/wupp-topic-maps/_wupp-topic-maps.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
+import ModalApplicationMenu from "react-cismap/topicmaps/menu/ModalApplicationMenu";
+
+import { action } from "@storybook/addon-actions";
+
+import { Introduction, Datengrundlage } from ".";
+
+const meta: Meta = {
+  title: "Wupp-topic-maps",
+};
+
+export default meta;
+
+export const IntroductionHitze: StoryObj = {
+  render: () => {
+    return <Introduction />;
+  },
+};
+
+export const ModalHelpAndInfoHitze: StoryObj = {
+  render: () => {
+    return (
+      <TopicMapContextProvider appKey="storybook-appkey">
+        <ModalApplicationMenu
+          menuSections={[
+            <Datengrundlage
+              setAppMenuActiveMenuSection={action(
+                "setAppMenuActiveMenuSection"
+              )}
+            />,
+          ]}
+          visible={true}
+        />
+      </TopicMapContextProvider>
+    );
+  },
+};
---
+++ src/wupp-topic-maps/index.tsx
@@ -0,0 +1,2 @@
+export { Help05Introduction as Introduction } from "./hitze/Help05Introduction";
+export { Help10Datengrundlage as Datengrundlage } from "./hitze/Help10Datengrundlage";
