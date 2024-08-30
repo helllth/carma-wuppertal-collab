4c6994e32c0f210d1a9446ab9673821582ce5e27
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-08-30 16:26:25 +0200

    add comman sections

---
+++ src/geoportal/help/Help15Datengrundlage.tsx
@@ -0,0 +1,25 @@
+import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
+
+const Component = () => {
+  return (
+    <GenericModalMenuSection
+      sectionKey="datengrundlage"
+      sectionTitle="Datengrundlagen"
+      sectionBsStyle="warning"
+      sectionContent={
+        <div>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+      }
+    />
+  );
+};
+export default Component;
