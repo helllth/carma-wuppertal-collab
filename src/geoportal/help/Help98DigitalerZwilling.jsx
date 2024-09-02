baaa360f8f0b54755d619cb239152257aaffab09
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-09-02 14:17:26 +0200

    fix digitaler zwilling section

--- src/geoportal/help/Help98DigitalerZwilling.jsx
+++ src/geoportal/help/Help98DigitalerZwilling.jsx
@@ -14,28 +14,25 @@ const Component = () => {
         <div>
           <p>
             <img
-              style={{ marginBottom: 0, marginLeft: 20, height: "120px" }}
-              target="_zwilling"
+              style={{ marginBottom: 0, marginLeft: 20, float: "right" }}
               alt="Logo DigiTal Zwilling"
               height="120"
-              align="right"
               src={Logo_DigiTalZwilling}
             />
             <div>
-              Die Hochwassergefahrenkarte basiert auf dem{" "}
-              <b>DigiTal Zwilling</b>, dem{" "}
+              Diese Anwendung basiert auf dem <b>DigiTal Zwilling</b>, dem{" "}
               <a
                 href="https://smart.wuppertal.de/projekte/digital-zwilling/digitaler-zwilling.php"
-                title={""}
+                title="DigiTal Zwilling"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Urbanen Digitalen Zwilling der Stadt Wuppertal
               </a>
-              . Sie stellt einen themenspezifischen Ausschnitt aus den Daten und
-              Methoden des DigiTal Zwillings für den ebenfalls spezifischen
-              Anwendungsfall "Hochwasser-Gefährdungsbeurteilung" bereit. Im
-              Konzept des DigiTal Zwillings implementiert die
-              Hochwassergefahrenkarte einen <b>Teilzwilling</b>, der dem{" "}
-              <b>Fachzwilling Klimawandel</b> zuzuordnen ist.
+              . Im Konzept des DigiTal Zwillings implementiert die Anwendung
+              einen <b>Teilzwilling</b>, also einen themenspezifischen
+              Ausschnitt aus den Daten und Methoden des DigiTal Zwillings für
+              einen spezifischen Anwendungsfall.
             </div>
           </p>
           <p>
@@ -44,14 +41,18 @@ const Component = () => {
             Wuppertaler Smart-City-Projekts{" "}
             <a
               href="https://smart.wuppertal.de/strategie/strategieprozess.php"
-              title={""}
+              title="Eine Smart City Strategie für Wuppertal"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               smart.wuppertal
             </a>
             . Das Projekt wird im Rahmen der dritten Session des Förderprogramms{" "}
             <a
               href="https://www.bmwsb.bund.de/Webs/BMWSB/DE/themen/stadt-wohnen/staedtebau/smart-cities/smart-cities-node.html"
-              title={""}
+              title="Smart Cities: Stadtentwicklung im digitalen Zeitalter"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Smart Cities made in Germany
             </a>{" "}
@@ -67,35 +68,35 @@ const Component = () => {
                   <a
                     href="https://www.bmwsb.bund.de/Webs/BMWSB/DE/startseite/startseite-node.html"
                     title={""}
+                    target="_zwilling"
                   >
                     <img
-                      target="_zwilling"
                       alt="Logo BMWSB"
                       height="120"
+                      style={{ height: "120px" }}
                       src={BMWSB_Logo}
                     />
                   </a>
                 </td>
                 <td style={{ textAlign: "left", paddingBottom: 10 }}>
-                  <a href="https://www.kfw.de/kfw.de.html" title={""}>
-                    <img
-                      target="_zwilling"
-                      alt="Logo KFW"
-                      height="120"
-                      src={KFW_Logo}
-                    />
+                  <a
+                    target="_zwilling"
+                    href="https://www.kfw.de/kfw.de.html"
+                    title={""}
+                  >
+                    <img alt="Logo KFW" height="120" src={KFW_Logo} />
                   </a>
                 </td>
               </tr>
             </tbody>
           </table>
           <p>
-            Fachspezifische Fragen, Hinweise und Anregungen zur
-            Hochwassergefahrenkarte richten Sie bitte an die E-Mail-Adresse{" "}
-            <a href="mailto:starkregen@stadt.wuppertal.de">
-              starkregen@stadt.wuppertal.de
-            </a>{" "}
-            mit dem Betreff <b>Hochwassergefahrenkarte Wuppertal</b>.
+            Fachspezifische Fragen, Hinweise und Anregungen zu dieser Anwendung
+            richten Sie bitte an die E-Mail-Adresse{" "}
+            <a href="mailto:smart@stadt.wuppertal.de">
+              smart@stadt.wuppertal.de
+            </a>
+            .
           </p>
         </div>
       }
