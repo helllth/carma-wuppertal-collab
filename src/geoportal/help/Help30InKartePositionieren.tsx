08b016c5e621efac78ccfd18e89d1c6955819674
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-09-02 14:28:26 +0200

    add in karte positionieren

--- src/geoportal/help/Help30InKartePositionieren.tsx
+++ src/geoportal/help/Help30InKartePositionieren.tsx
@@ -1,4 +1,6 @@
 import GenericModalMenuSection from "react-cismap/topicmaps/menu/Section";
+import { InKartePositionierenText } from "../../commons";
+import Icon from "react-cismap/commons/Icon";
 
 const Component = () => {
   return (
@@ -8,14 +10,28 @@ const Component = () => {
       sectionBsStyle="info"
       sectionContent={
         <div>
-          Could now she how in was sleep dropped in by back shall come into
-          needed wild that picture they to have would by her succeeding, hair a
-          with the I first should expected line of so got her solitary the and
-          that coffee that omens, treble-range are quitting for high line text,
-          parts else. A have the unprofitable both the leather at neighbours in
-          on each written and assignment. When me the his degree read. She that
-          the equation, of by hand. Select picture spare not got in social and
-          parameters absolutely he and that if the alphabet carpeting of.
+          <InKartePositionierenText
+            pretext={
+              <>
+                <p>
+                  Um eine bestimmte Stelle des Stadtgebietes zu erkunden, geben
+                  Sie den Anfang eines Stadtteils (Stadtbezirk oder Quartier),
+                  einer Adresse, eines Straßennamens oder eines POI im
+                  Eingabefeld links unten ein (mindestens 2 Zeichen). In der
+                  inkrementellen Auswahlliste werden Ihnen passende Treffer
+                  angeboten. (Wenn Sie weitere Zeichen eingeben, wird der Inhalt
+                  der Auswahlliste angepasst.) Durch das vorangestellte Symbol
+                  erkennen Sie, ob es sich dabei um einen <Icon name="circle" />{" "}
+                  Stadtbezirk, ein <Icon name="pie-chart" /> Quartier, eine{" "}
+                  <Icon name="home" /> Adresse, eine <Icon name="road" /> Straße
+                  ohne zugeordnete Hausnummern, einen <Icon name="tag" /> POI,
+                  die <Icon name="tags" /> alternative Bezeichnung eines POI,
+                  eine <Icon name="child" /> Kindertageseinrichtung oder eine{" "}
+                  <Icon name="graduation-cap" /> Schule handelt.
+                </p>
+              </>
+            }
+          />
         </div>
       }
     />
