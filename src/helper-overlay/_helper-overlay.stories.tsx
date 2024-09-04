80e49aa36f27c8165f2f82e0bae9837cb124787e
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-09-04 11:57:28 +0200

    add elements object

---
+++ src/helper-overlay/_helper-overlay.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  title: "Helper overlay",
+};
+
+export default meta;
+
+export const TextInSearchPlaceholder: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
