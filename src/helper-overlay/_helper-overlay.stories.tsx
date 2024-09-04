778abf0ca9e23b5baa50ad13e5f245987a71e184
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-09-04 18:20:44 +0200

    add get collab help config func

--- src/helper-overlay/_helper-overlay.stories.tsx
+++ src/helper-overlay/_helper-overlay.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { getCollabedHelpComponentConfig } from ".";
 
 const meta: Meta = {
   title: "Helper overlay",
@@ -6,7 +7,63 @@ const meta: Meta = {
 
 export default meta;
 
-export const TextInSearchPlaceholder: StoryObj = {
+export const Menüleiste: StoryObj = {
+  render: () => {
+    const element = getCollabedHelpComponentConfig("VOLLBILD");
+
+    return <>{element?.primary.conten}</>;
+  },
+};
+
+export const LayerButtons: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Hintergrund: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Menü: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Zoom: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Vollbild: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const MinePosition: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Rathaus: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const Messungen: StoryObj = {
+  render: () => {
+    return <div>Text</div>;
+  },
+};
+
+export const GazetteerSuche: StoryObj = {
   render: () => {
     return <div>Text</div>;
   },
---
+++ src/helper-overlay/index.tsx
@@ -0,0 +1,90 @@
+export const elements = {
+  menuTour: {
+    key: "MENULEISTE",
+    containerPos: "center",
+    contentPos: "center",
+    content: <div>Menüleiste</div>,
+  },
+  layerWrapperTourRef: {
+    message: "LAYERBUTTONS",
+    options: {
+      containerPos: "center",
+      contentPos: "center",
+      content: <div>Layer Buttons</div>,
+    },
+  },
+  hintergrundTour: {
+    key: "HINTERGRUND",
+    conetn: <div>Hintergrund</div>,
+    containerPos: "center",
+    contentPos: "center",
+  },
+  modalMenuTourRef: {
+    key: "MENU",
+    containerPos: "center",
+    contentPos: "center",
+    message: <div>Menü</div>,
+  },
+  zoomControlTour: {
+    key: "ZOOM",
+    content: <div>Zoom</div>,
+    containerPos: "center",
+    contentPos: "left-center",
+  },
+  fullScreenControlTour: {
+    key: "VOLLBILD",
+    conten: <div>Vollbild</div>,
+    containerPos: "center",
+    contentPos: "left-center",
+  },
+  navigatorControlTour: {
+    key: "MINE_POSITION",
+    content: <div>Mine Position</div>,
+    containerPos: "center",
+    contentPos: "left-center",
+  },
+  homeControlTour: {
+    key: "RATHAUS",
+    content: <div>Rathaus</div>,
+    containerPos: "center",
+    contentPos: "left-center",
+  },
+  measurementControlTour: {
+    key: "MESSUNGEN",
+    content: <div>Messungen</div>,
+    containerPos: "center",
+    contentPos: "left-center",
+  },
+  gazetteerControlTour: {
+    key: "GAZETTEER_SUCHE",
+    content: "Gazetteer Suche",
+    containerPos: "center",
+    contentPos: "center",
+  },
+};
+
+export const getCollabedHelpComponentConfig = (keyId) => {
+  for (const k in elements) {
+    if (elements[k].key === keyId) {
+      return {
+        primary: {
+          ...elements[k],
+        },
+      };
+    }
+  }
+  return undefined;
+};
+
+// export const getCollabedHelpComponentConfig = (key) => {
+//   if (key === "LAYERBUTTONS") {
+//     return {
+//       primary: {
+//         contentPos: "UPPER",
+//         content: <div>Layer Buttons</div>,
+//       },
+//       secondary: {content: <Popover>Lsdlkgjlksdjkldfjlkgfdjayer Buttons</Popover>},
+//     };
+//   }
+//   return undefined;
+// };
