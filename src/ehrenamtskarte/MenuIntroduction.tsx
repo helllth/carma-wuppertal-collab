a21714ba230e146ca5b780aed6e80860d8cfcc23
default <frederic.moebis@stadt.wuppertal.de>
2024-09-09 09:12:03 +0000

    Textänderungen Ehrenamtskarte

--- src/ehrenamtskarte/MenuIntroduction.tsx
+++ src/ehrenamtskarte/MenuIntroduction.tsx
@@ -42,7 +42,19 @@ export const MenuIntroduction: React.FC<MenuIntroductionProps> = ({
       >
         Kompaktanleitung
       </Link>{" "}
-      für detailliertere Bedienungsinformationen.
+      für detailliertere Bedienungsinformationen und{" "}
+      <Link
+        id="lnkUDZ"
+        to="digiTal"
+        containerId="myMenu"
+        smooth={true}
+        delay={100}
+        onClick={() => setAppMenuActiveMenuSection("digiTal")}
+        className="renderAsLink"
+      >
+        Urbaner Digitaler Zwilling
+      </Link>{" "}
+      für eine Einordnung der Ehrenamtskarte in das Gesamtsystem des Digitalen Zwillings der Stadt Wuppertal.
     </span>
   );
 };
