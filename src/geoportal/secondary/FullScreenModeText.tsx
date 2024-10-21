198d56606076e67617cff8a5a74ee79d586b8a00
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-10-21 09:17:35 +0200

    add a link to full screen

--- src/geoportal/secondary/FullScreenModeText.tsx
+++ src/geoportal/secondary/FullScreenModeText.tsx
@@ -1,16 +1,18 @@
+import { Link } from "../../helper-overlay/components/Link";
+
 export const FullScreenModeText = () => {
-    return (
-      <ul
-        className="overlay-helper-ul-class"
-        style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
-      >
-        <div>
-          Wechselschalter zum An-/Abschalten des Vollbildmodus (2D und 3D) | 
-          Ausblenden der Bedienungselemente des Browsers erzeugt maximalen Platz für das Geoportal | 
-          Vollbildmodus kombinierbar mit <Link target="MESSUNGEN">Modus Strecke/Fläche messen</Link> oder Modus Sachdaten abfragen | 
-          Beenden auch mit esc-Taste
-        </div>
-      </ul>
-    );
-  };
-  
+  return (
+    <ul
+      className="overlay-helper-ul-class"
+      style={{ margin: "0", paddingLeft: "20px", maxWidth: "600px" }}
+    >
+      <div>
+        Wechselschalter zum An-/Abschalten des Vollbildmodus (2D und 3D) |
+        Ausblenden der Bedienungselemente des Browsers erzeugt maximalen Platz
+        für das Geoportal | Vollbildmodus kombinierbar mit{" "}
+        <Link target="MESSUNGEN">Modus Strecke/Fläche messen</Link> oder Modus
+        Sachdaten abfragen | Beenden auch mit esc-Taste
+      </div>
+    </ul>
+  );
+};
