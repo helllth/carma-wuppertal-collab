e3bd47e679869430d1562ad613d4a8c973d080f2
helllth <helllth@users.noreply.github.com>
2024-10-24 08:45:59 +0000

    deploy: 6d2b7c3b928adec422012c5506eefd5db6c89409

---
+++ assets/_geoportal.modals.stories-Cmi6ua8A.js
@@ -0,0 +1,35 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{E as s}from"./EMobiModal-c8vZmT1p.js";import"./index-CTjT7uj6.js";import"./index-CeejqomF.js";import"./index-BRV0Se7Z.js";import"./Card-sW1K-9N4.js";import"./inheritsLoose-D4WLrkXa.js";import"./index-BqsmnRTQ.js";import"./Modal-C4MwPEwt.js";const h={title:"Geoportal/Sachdatenabfrage"},e={render:()=>n.jsx(s,{feature:{properties:{standort:"Hardt / Elisenhöhe",adresse:"Otto-Schell-Weg 18",status:"true",foto:"https://www.wuppertal.de/geoportal/emobil/autos/fotos/hardt_elisenhoehe.jpg",zusatzinfo:"Parkplatz Hardt / Elisenhöhe",bemerkung:"Erste Parkreihe nach der Einfahrt.",versatz:"1",vorhandene_stecker:"true",stecker1:"https://www.wuppertal.de/geoportal/emobil/images-autos/Schuko_plug ! 2 x Schuko Steckdose (3.7kW)",stecker2:"https://www.wuppertal.de/geoportal/emobil/images-autos/Type_2_mennekes ! 2 x Typ 2 Steckdose (22kW)",parkgebuehr:"Während des Ladevorgangs max. 4 Std. kostenfrei. Der Ladeplatz ist kein Parkplatz.",zugang1:"App (Ladepay)",zugang2:"Ladekarte (WSW eMobil flat)",zugang3:"Ladekarte für den Verbund ladenetz.de",anzahl:"4",abrechnung:"https://www.wsw-online.de/fileadmin/Energie-Wasser/Nachhaltigkeit/Klimafonds_Antrag/2016_07_Preisblatt-WSW_eMOBIL_FLAT.pdf",offen:"https://www.wuppertal.de/geoportal/emobil/images-autos/pikto_24-7.png",strom:"AC",betreiber:"WSW Energie & Wasser AG - eMobilität",betreiberadresse:"Bromberger Straße 39-41",betreiberort:"42281 Wuppertal",betreiberhomepage:"https://www.wsw-online.de/wsw-energie-wasser/privatkunden/nachhaltigkeit/emobilitaet/",betreiberemail:"emobil@wsw-online.de",betreibertelefon:"+49-202-5695140",gruen:"https://www.wuppertal.de/geoportal/emobil/images-autos/pikto_gruener_strom.png",signatur:"svg-signaturen/pikto_e-auto_blau_freigestellt_2.svg",signatur2:"svg-signaturen/pikto_e-auto_blau_freigestellt_3.svg"}},setOpen:()=>{}})};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => {
+    return <SecondaryInfoModal feature={{
+      properties: {
+        standort: "Hardt / Elisenhöhe",
+        adresse: "Otto-Schell-Weg 18",
+        status: "true",
+        foto: "https://www.wuppertal.de/geoportal/emobil/autos/fotos/hardt_elisenhoehe.jpg",
+        zusatzinfo: "Parkplatz Hardt / Elisenhöhe",
+        bemerkung: "Erste Parkreihe nach der Einfahrt.",
+        versatz: "1",
+        vorhandene_stecker: "true",
+        stecker1: "https://www.wuppertal.de/geoportal/emobil/images-autos/Schuko_plug ! 2 x Schuko Steckdose (3.7kW)",
+        stecker2: "https://www.wuppertal.de/geoportal/emobil/images-autos/Type_2_mennekes ! 2 x Typ 2 Steckdose (22kW)",
+        parkgebuehr: "Während des Ladevorgangs max. 4 Std. kostenfrei. Der Ladeplatz ist kein Parkplatz.",
+        zugang1: "App (Ladepay)",
+        zugang2: "Ladekarte (WSW eMobil flat)",
+        zugang3: "Ladekarte für den Verbund ladenetz.de",
+        anzahl: "4",
+        abrechnung: "https://www.wsw-online.de/fileadmin/Energie-Wasser/Nachhaltigkeit/Klimafonds_Antrag/2016_07_Preisblatt-WSW_eMOBIL_FLAT.pdf",
+        offen: "https://www.wuppertal.de/geoportal/emobil/images-autos/pikto_24-7.png",
+        strom: "AC",
+        betreiber: "WSW Energie & Wasser AG - eMobilität",
+        betreiberadresse: "Bromberger Straße 39-41",
+        betreiberort: "42281 Wuppertal",
+        betreiberhomepage: "https://www.wsw-online.de/wsw-energie-wasser/privatkunden/nachhaltigkeit/emobilitaet/",
+        betreiberemail: "emobil@wsw-online.de",
+        betreibertelefon: "+49-202-5695140",
+        gruen: "https://www.wuppertal.de/geoportal/emobil/images-autos/pikto_gruener_strom.png",
+        signatur: "svg-signaturen/pikto_e-auto_blau_freigestellt_2.svg",
+        signatur2: "svg-signaturen/pikto_e-auto_blau_freigestellt_3.svg"
+      }
+    }} setOpen={() => {}} />;
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const m=["EMobilität"];export{e as EMobilität,m as __namedExportsOrder,h as default};
